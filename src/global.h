8c026b7bd9cb7a45aecfb7aa105461851aa4e3ac
Morten Kristensen <msk@nullpointer.dk>
2014-12-10 22:40:22 +0100

    Implemented global.h with B_BEGIN_NAMEPACE and B_END_NAMESPACE.

---
+++ src/global.h
@@ -0,0 +1,10 @@
+#ifndef B_GLOBAL_H
+#define B_GLOBAL_H
+
+#define BURATOR_NAMESPACE burator
+#define BLURATOR_NAMESPACE blurator
+
+#define B_BEGIN_NAMEPACE namespace BURATOR_NAMESPACE { namespace BLURATOR_NAMESPACE {
+#define B_END_NAMESPACE } }
+
+#endif // B_GLOBAL_H
