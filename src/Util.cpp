5d2da0ffb02b2062e1b6c5d2615af632b612924a
Morten Kristensen <msk@nullpointer.dk>
2014-12-11 00:12:15 +0100

    Implemented Util.h/cpp which has functions to convert image data into OpenCV format.

---
+++ src/Util.cpp
@@ -0,0 +1,48 @@
+#include <QFile>
+#include <QImage>
+#include <QString>
+#include <QBuffer>
+#include <QByteArray>
+
+#include <vector>
+
+#include "Util.h"
+
+B_BEGIN_NAMEPACE
+
+MatPtr Util::imageToMat(const QString &file) {
+  QFile f(file);
+  if (!f.open(QIODevice::ReadOnly)) {
+    return nullptr;
+  }
+  return imageToMat(f.readAll());
+}
+
+MatPtr Util::imageToMat(const QImage &image, const char *fmt) {
+  QByteArray arr;
+  QBuffer buf(&arr);
+  buf.open(QIODevice::WriteOnly);
+  image.save(&buf, fmt);
+  return imageToMat(arr);
+}
+
+MatPtr Util::imageToMat(const QByteArray &data) {
+  return imageToMat(data.constData(), data.size());
+}
+
+MatPtr Util::imageToMat(const char *data, int len) {
+  if (!data || len <= 0) {
+    return nullptr;
+  }
+
+  std::vector<char> vec(data, data + len);
+  if (vec.size() == 0) {
+    return nullptr;
+  }
+
+  auto mat = MatPtr(new cv::Mat);
+  imdecode(cv::Mat(vec), CV_LOAD_IMAGE_COLOR, mat.get());
+  return mat;
+}
+
+B_END_NAMESPACE
---
+++ src/Util.h
@@ -0,0 +1,23 @@
+#ifndef B_UTIL_H
+#define B_UTIL_H
+
+#include "Types.h"
+#include "Global.h"
+
+class QImage;
+class QString;
+class QByteArray;
+
+B_BEGIN_NAMEPACE
+
+class Util {
+public:
+  static MatPtr imageToMat(const QString &file);
+  static MatPtr imageToMat(const QImage &image, const char *fmt = "PNG");
+  static MatPtr imageToMat(const QByteArray &data);
+  static MatPtr imageToMat(const char *data, int len);
+};
+
+B_END_NAMESPACE
+
+#endif // B_UTIL_H
