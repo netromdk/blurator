c55141b18ebaefd0d62f13b323771ff68b1b5a8c
Brandon Davis <dbrandon.91@gmail.com>
2014-12-10 14:15:25 -0800

    Add Qt5Gui, and a very simple argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,31 @@
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QStringList>
+#include <QString>
+#include <QDebug>
+
 int main(int argc, char **argv) {
+	QCoreApplication app(argc, argv);
+	QCoreApplication::setApplicationName("Blurator");
+	QCoreApplication::setApplicationVersion("0.1");
+
+  QCommandLineParser parser;
+  parser.setApplicationDescription("Blur license plates and faces");
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addPositionalArgument("image", QCoreApplication::translate("main",
+  	                        "Path to images or image"));
+
+  parser.process(app);
+  const QStringList args = parser.positionalArguments();
+
+  // ensure the arguments are passed
+  if (args.isEmpty()) {
+  	qDebug() << "Must provide path to images!";
+  	// should exit here!!
+  } 
+  QString path = args.at(0);
+
+  // use path...
   return 0;
 }
