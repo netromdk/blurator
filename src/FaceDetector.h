ac188a5f7eb4462dd39cb181afcb8d004dafc4a5
Morten Kristensen <msk@nullpointer.dk>
2014-12-12 16:51:42 +0100

    Implemented a face detector that yields a list of faces.

---
+++ src/FaceDetector.h
@@ -0,0 +1,30 @@
+#ifndef MOCAM_FACE_DETECTOR_H
+#define MOCAM_FACE_DETECTOR_H
+
+#include <QList>
+
+#include <opencv2/opencv.hpp>
+
+#include "Face.h"
+#include "Types.h"
+#include "Global.h"
+
+class QString;
+
+B_BEGIN_NAMESPACE
+
+class FaceDetector {
+public:
+  FaceDetector(const QString &faceCascade, const QString &eyesCascade);
+
+  bool isValid() const { return valid; }
+  QList<FacePtr> detect(const MatPtr image);
+
+private:
+  bool valid;
+  cv::CascadeClassifier faceCas, eyesCas;
+};
+
+B_END_NAMESPACE
+
+#endif // MOCAM_FACE_DETECTOR_H
