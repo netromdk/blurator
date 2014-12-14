f9d32c61f62adc137817243f795582a257497446
Morten Kristensen <morten@burator.com>
2014-12-14 19:13:03 +0100

    Defined PASS and FAIL to be more explicit in tests.

--- test/Test.h
+++ test/Test.h
@@ -1,3 +1,9 @@
+#ifndef B_TEST_H
+#define B_TEST_H
+
+#define PASS 0
+#define FAIL 1
+
 /**
  * Constructor sets up a testing environment to use in test cases, and
  * desconstructor cleans up everything.
@@ -8,3 +14,4 @@ public:
   ~TestEnv();
 };
 
+#endif // B_TEST_H
