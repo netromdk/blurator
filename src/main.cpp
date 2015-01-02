d1aa984288947dd3a8662a6eaa414344a77d3c65
Brandon Davis <brandon@burator.com>
2015-01-01 20:02:56 -0800

    Implement commandline option for faces and plates, with error handling.

--- src/main.cpp
+++ src/main.cpp
@@ -27,15 +27,22 @@ int main(int argc, char **argv) {
   parser.addVersionOption();
   parser.addPositionalArgument("path", QCoreApplication::translate("main",
   	                        "Path to images or image"));
+  // Face detection option
   QCommandLineOption detectFaces(QStringList() << "f" << "faces",
           QCoreApplication::translate("main", "Detect faces"));
-
   parser.addOption(detectFaces);
+
+  // License plate detection option
+  QCommandLineOption detectPlates(QStringList() << "p" << "plates",
+          QCoreApplication::translate("main", "Detect license plates"));
+  parser.addOption(detectPlates);
+  
   parser.process(app);
   const QStringList args = parser.positionalArguments();
 
+  // Check optional command line options
   bool dFaces = parser.isSet(detectFaces);
-  qDebug() << dFaces;
+  bool dPlates = parser.isSet(detectPlates);
 
   // ensure the arguments are passed
   if (args.isEmpty()) {
@@ -45,51 +52,62 @@ int main(int argc, char **argv) {
   QString path = args.at(0);
 
 
-  /* Testing */
-  QFile f(path);
-  if (!f.open(QIODevice::ReadOnly)) {
-    qCritical() << "Could not read from image file!";
-    return -1;
-  }
-  QByteArray imageData = f.readAll();
-  f.close();
-
-  qDebug() << "Read" << imageData.size() << "bytes";
-
-  MatPtr image = Util::imageToMat(imageData);
-  if (image == nullptr) {
-    qCritical() << "Invalid image!";
-    return -1;
-  }
-  qDebug() << "Loaded image..";
-
-  FaceDetector detector;
-  if (!detector.isValid()) {
-    qCritical() << "Could not setup facial detector.";
-    return -1;
-  }
-
-  qDebug() << "Detecting faces..";
-  QList<FacePtr> faces = detector.detect(image);
-  if (faces.isEmpty()) {
-    qDebug() << "Did not find any faces!";
-    return 0;
-  }
-
-  qDebug() << "Found" << faces.size() << "face(s).";
-  qDebug() << faces;
-
-  // Render face overlays to an image file to visualize the result.
-  if (!faces.isEmpty()) {
-    QImage overlay = QImage::fromData(imageData);
-    Util::drawFaces(overlay, faces);
-    QString overlayFile = path + ".overlay.png";
-    if (overlay.save(overlayFile)) {
-      qDebug() << "Saved overlays to:" << overlayFile;
+  /* testing */
+  if (dFaces) {
+    QFile f(path);
+    if (!f.open(QIODevice::ReadOnly)) {
+      qCritical() << "Could not read from image file!";
+      return -1;
     }
-    else {
-      qCritical() << "Could not save overlays";
+    QByteArray imageData = f.readAll();
+    f.close();
+
+    qDebug() << "Read" << imageData.size() << "bytes";
+
+    MatPtr image = Util::imageToMat(imageData);
+    if (image == nullptr) {
+      qCritical() << "Invalid image!";
+      return -1;
     }
+    qDebug() << "Loaded image..";
+
+    FaceDetector detector;
+    if (!detector.isValid()) {
+      qCritical() << "Could not setup facial detector.";
+      return -1;
+    }
+
+    qDebug() << "Detecting faces..";
+    QList<FacePtr> faces = detector.detect(image);
+    if (faces.isEmpty()) {
+      qDebug() << "Did not find any faces!";
+      return 0;
+    }
+
+    qDebug() << "Found" << faces.size() << "face(s).";
+    qDebug() << faces;
+
+    // Render face overlays to an image file to visualize the result.
+    if (!faces.isEmpty()) {
+      QImage overlay = QImage::fromData(imageData);
+      Util::drawFaces(overlay, faces);
+      QString overlayFile = path + ".overlay.png";
+      if (overlay.save(overlayFile)) {
+        qDebug() << "Saved overlays to:" << overlayFile;
+      }
+      else {
+        qCritical() << "Could not save overlays";
+      }
+    }
+  } 
+  if(dPlates) {
+    qCritical() << "Plate detection not implemented yet!";
+    return -1;
+  } 
+
+  if (!dPlates && !dFaces) {
+    qCritical() << "Nothing to do on images!";
+    return -1;
   }
   return 0;
 }
