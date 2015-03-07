d90d005b9168c2b7e583acce85de28e7b6b16434
Morten Kristensen <morten@burator.com>
2015-03-07 21:12:48 +0100

    #15: Added paths for finding Qt and OpenCV on Windows.

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,6 +1,6 @@
 # Qt 5
+# Added some paths to help find the modules on some systems.
 if (NOT WIN32)
-  # Added some paths to help find the modules on some systems.
   set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
     "/usr/local/opt"
     "/usr/local/opt/qt5"
@@ -9,6 +9,9 @@ if (NOT WIN32)
     "/usr/local/Qt-5.3.1"
     "/usr/local/Qt-5.3.0"
     "/usr/local/Qt-5.2.1")
+else()
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
+    "C:/qt-everywhere-opensource-src-5.4.0-w64/qtbase/lib/cmake")
 endif()
 
 set(CMAKE_AUTOMOC ON) # moc when necessary.
@@ -35,4 +38,8 @@ else()
 endif()
 
 # OpenCV
+if (WIN32)
+  set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH}
+    "C:/opencv/build")
+endif()
 find_package(OpenCV REQUIRED)
