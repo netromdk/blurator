9554911559f3ee2b45afc8e60cbb92170ffa2ad8
Morten Kristensen <morten@burator.com>
2015-03-08 20:55:37 +0100

    Linux: Implemented so 'dist' target works by creating the distribution.

--- scripts/makedist.sh
+++ scripts/makedist.sh
@@ -9,32 +9,46 @@ ZIP_DIST=`echo $3 | sed 's/ /\\\\ /g'`
 PY_EXEC=`echo $4 | sed 's/ /\\\\ /g'`
 QT_PLUGINS=`echo $5 | sed 's/ /\\\\ /g'`
 
+OS=`uname`
+
 eval mkdir -p ${DIST_DIR}
 BINARY_IN=${BIN_DIR}/blurator
 BINARY_OUT=${DIST_DIR}/blurator
 
 # Copy and manage dependencies.
-${PY_EXEC} scripts/copy_deps.py ${BINARY_IN} ${DIST_DIR}
-if [ ! $? -eq 0 ]; then
-   echo "Failed to copy deps!"
-   exit -1
+if [ ${OS} = "Darwin" ]; then
+    ${PY_EXEC} scripts/copy_deps.py ${BINARY_IN} ${DIST_DIR}
+    if [ ! $? -eq 0 ]; then
+        echo "Failed to copy deps!"
+        exit -1
+    fi
+else
+    eval cp ${BINARY_IN} ${BINARY_OUT}
+    eval scripts/copyshared.sh ${BINARY_OUT} ${DIST_DIR}
 fi
 
-chmod +x ${BINARY_OUT}
+eval chmod +x ${BINARY_OUT}
 
 # Copy Qt plugins.
 eval mkdir -p ${DIST_DIR}/imageformats
-eval cp ${QT_PLUGINS}/imageformats/libqjpeg.dylib \
-     ${QT_PLUGINS}/imageformats/libqgif.dylib \
-     ${QT_PLUGINS}/imageformats/libqtiff.dylib \
-     ${DIST_DIR}/imageformats/
-for plugin in ${DIST_DIR}/imageformats/*; do
-    eval ${PY_EXEC} scripts/fix_binary.py ${plugin}
-done
+if [ ${OS} = "Darwin" ]; then
+    eval cp ${QT_PLUGINS}/imageformats/libqjpeg.dylib \
+         ${QT_PLUGINS}/imageformats/libqgif.dylib \
+         ${QT_PLUGINS}/imageformats/libqtiff.dylib \
+         ${DIST_DIR}/imageformats/
+    for plugin in ${DIST_DIR}/imageformats/*; do
+        eval ${PY_EXEC} scripts/fix_binary.py ${plugin}
+    done
+else
+    eval cp ${QT_PLUGINS}/imageformats/libqjpeg.so \
+         ${QT_PLUGINS}/imageformats/libqgif.so \
+         ${QT_PLUGINS}/imageformats/libqtiff.so \
+         ${DIST_DIR}/imageformats/
+fi
 
 # Create zip file.
 eval echo "Creating zip file: ${ZIP_DIST}"
-cd ${BIN_DIR}
+eval cd ${BIN_DIR}
 rm -f dist.zip
 eval zip -r9q dist.zip ${DIST_BASE}
 eval mv dist.zip ${ZIP_DIST}
